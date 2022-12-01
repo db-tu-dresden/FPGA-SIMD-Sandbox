7fae530e78f132b4700e5282aaa637a43d0f07ce
eric-stange <Eric.Stange@mailbox.tu-dresden.de>
2022-12-01 23:18:18 +0100

    Update build_fpga_emu.sh
    Add source path that the emulation can be build as a batchjob.

--- build_fpga_emu.sh
+++ build_fpga_emu.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 tools_setup -t S10OAPI
+source /data/intel_fpga/devcloudLoginToolSetup.sh
+source /opt/intel/inteloneapi/setvars.sh 
 source /glob/development-tools/versions/oneapi/2022.3/oneapi/setvars.sh --force
 make emu 
