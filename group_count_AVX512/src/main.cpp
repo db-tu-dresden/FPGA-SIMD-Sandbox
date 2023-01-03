1f50f61602feaf821acf7da5865427b5378cd512
dhabich <dirk.habich@tu-dresden.de>
2023-01-03 13:01:43 +0100

    changed

--- group_count_AVX512/src/main.cpp
+++ group_count_AVX512/src/main.cpp
@@ -7,9 +7,11 @@
 #include <chrono>
 
 #include "LinearProbing_avx512.cpp"
+//#include "LinearProbing_avx512-v1.cpp"
 #include "LinearProbing_scalar.cpp"
 /*
 *   This is a proprietary AVX512 implementation of Bala Gurumurthy's LinearProbing approach. 
+ 
 *   The logical approach refers to the procedure described in the paper.
 *   Link to paper "SIMD Vectorized Hashing for Grouped Aggregation"
 *   https://wwwiti.cs.uni-magdeburg.de/iti_db/publikationen/ps/auto/Gurumurthy:ADBIS18.pdf
@@ -22,10 +24,10 @@
  * @param scale multiplier to determine the value of the HSIZE (note "1.6" corresponds to 60% more slots in the hashVec[] than there are distinctValues 
  * @param HSIZE HashSize (corresponds to size of hashVec[] and countVec[])
  */
-uint64_t distinctValues = 8000;
+uint64_t distinctValues = 60;
 uint64_t dataSize = 16*10000000;
-float scale = 1.1;
-uint64_t HSIZE = distinctValues * scale;
+float scale = 1.4;
+uint64_t HSIZE = distinctValues*scale;
 
 
 int  main(int argc, char** argv){
@@ -36,6 +38,7 @@ int  main(int argc, char** argv){
      * allocate memory for data input array and fill with random numbers
      */
     uint32_t *arr;
+    
     arr = (uint32_t *) aligned_alloc(64,dataSize * sizeof(uint32_t));
     if (arr != NULL) {
         cout << "Memory allocated - " << dataSize << " values, between 1 and " << distinctValues << endl;
@@ -58,53 +61,68 @@ int  main(int argc, char** argv){
         cout << "HashTable not allocated" << endl;
     }
 
-    /** ##############################################
-     * Linear Probing AVX512
-    */
+
     initializeHashMap(hashVec,countVec,HSIZE);
-    cout <<"Linear Probing with AVX512"<<endl;
+    cout <<"=============================="<<endl;
+    cout <<"Linear Probing - scalar"<<endl;
     auto begin = chrono::high_resolution_clock::now();
-    LinearProbingAVX512Variant3(arr, dataSize, hashVec, countVec, HSIZE);
+    LinearProbingScalar(arr, dataSize, hashVec, countVec, HSIZE);
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
 
     auto mis = (dataSize/1000000)/((double)duration/(double)((uint64_t)1*(uint64_t)1000000000));
     cout<<mis<<endl;
+    validate(dataSize, hashVec,countVec, HSIZE);
 
-    // Ausgabe des HashTable - AVX512 Variante
-    uint32_t sumAVX;
-    for (int i=0; i<distinctValues * scale; i++) {
-        if (hashVec[i]>0) {
-            sumAVX+=countVec[i];
-        }
-    }
-    cout << "Final result check: AVX512 approach"<<endl;
-    cout << "Compare parameter dataSize against sum of all count values in countVec:"<<endl;
-    cout << dataSize <<" "<<sumAVX<<endl;
-     
-    /** ##############################################
-     * Linear Probing scalar
-    */
+
     initializeHashMap(hashVec,countVec,HSIZE);
+    cout <<"=============================="<<endl;
+    cout <<"Linear Probing - SIMD Variant1"<<endl;
     begin = chrono::high_resolution_clock::now();
-    cout <<"Linear Probing scalar"<<endl;
-    LinearProbingScalar(arr, dataSize, hashVec, countVec, HSIZE);
+    LinearProbingAVX512Variant1(arr, dataSize, hashVec, countVec, HSIZE);
     end = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+
     mis = (dataSize/1000000)/((double)duration/(double)((uint64_t)1*(uint64_t)1000000000));
+    cout<<mis<<endl;
+    validate(dataSize, hashVec,countVec, HSIZE);
+
+    initializeHashMap(hashVec,countVec,HSIZE);
+    cout <<"=============================="<<endl;
+    cout <<"Linear Probing - SIMD Variant2"<<endl;
+    begin = chrono::high_resolution_clock::now();
+    LinearProbingAVX512Variant2(arr, dataSize, hashVec, countVec, HSIZE);
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+
+    mis = (dataSize/1000000)/((double)duration/(double)((uint64_t)1*(uint64_t)1000000000));
+    cout<<mis<<endl;
+    validate(dataSize, hashVec,countVec, HSIZE);
+
+    initializeHashMap(hashVec,countVec,HSIZE);
+    cout <<"=============================="<<endl;
+    cout <<"Linear Probing - SIMD Variant3"<<endl;
+    begin = chrono::high_resolution_clock::now();
+    LinearProbingAVX512Variant3(arr, dataSize, hashVec, countVec, HSIZE);
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+
+    mis = (dataSize/1000000)/((double)duration/(double)((uint64_t)1*(uint64_t)1000000000));
+    cout<<mis<<endl;
+    validate(dataSize, hashVec,countVec, HSIZE);
+     
+    
+    /*initializeHashMap(hashVec,countVec,HSIZE);
+     begin = chrono::high_resolution_clock::now();
+    cout <<"Linear Probing scalar"<<endl;
+    //unorderedMap(arr, dataSize, 1600);
+    SOTASIMD(arr,dataSize,hashVec,countVec,HSIZE);
+   // LinearProbingAVX512Scalar()
+     end = std::chrono::high_resolution_clock::now();
+     duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+     mis = (dataSize/1000000)/((double)duration/(double)((uint64_t)1*(uint64_t)1000000000));
 
     cout <<mis<<endl;
-
-    // Ausgabe des HashTable - AVX512 Variante
-    uint32_t sumLinear;
-    for (int i=0; i<distinctValues * scale; i++) {
-        if (hashVec[i]>0) {
-            sumLinear+=countVec[i];
-        }
-    }
-    cout << "Final result check: scalar approach"<<endl;
-    cout << "Compare parameter dataSize against sum of all count values in countVec:"<<endl;
-    cout << dataSize <<" "<<sumLinear<<endl;
     
     /*
     //__m512i a = _mm512_setzero_epi32();
@@ -118,7 +136,17 @@ int  main(int argc, char** argv){
     //__mmask16 mask1 = _mm512_knot(mask);
 
     cout <<32-__builtin_clz(mask)<<endl;
-    */    
+    */
+    // Ausgabe des HashTable
+   /* uint32_t sum=0;
+    for (int i=0; i<distinctValues * scale; i++) {
+        if (hashVec[i]>0) {
+            sum+=countVec[i];
+        }
+    }
+    cout << "Final result check: compare parameter dataSize against sum of all count values in countVec:"<<endl;
+    cout << dataSize <<" "<<sum<<endl;*/
+    
     return 0;
 
 }
