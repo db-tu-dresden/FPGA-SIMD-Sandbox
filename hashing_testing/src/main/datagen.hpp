3a7e412ad8fc560706b119130e82ace696489e88
Lennart Schmidt <lennart.schmidt1@tu-dresden.de>
2023-01-26 10:41:08 +0100

    non working update

--- hashing_testing/src/main/datagen.hpp
+++ hashing_testing/src/main/datagen.hpp
@@ -61,6 +61,22 @@ std::string generation_to_string(Generation z){
     return "unknown";
 }
 
+
+enum Alignment{UNALIGNED, BAD, GOOD};
+
+std::string alignment_to_string(Alignment x){
+    switch(x){
+        case Alignment::UNALIGNED:
+            return "unaligned";
+        case Alignment::BAD:
+            return "bad";
+        case Alignment::GOOD:
+            return "good";
+    }
+    return "unknown";
+}
+
+
 template<typename T>
 T make_grid(size_t x){
     size_t halfword[] ={0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE};
@@ -91,23 +107,10 @@ uint64_t noise(size_t position, size_t seed){
     mangled *= BIT_NOISE1;
     mangled += seed;
     mangled ^= (mangled << 13);
-    // mangled ^= (mangled >> 8);
     mangled += BIT_NOISE2;
     mangled ^= (mangled >> 7);
-    // mangled ^= (mangled << 8);
     mangled *= BIT_NOISE3;
     mangled ^= (mangled << 17);
-    // mangled ^= (mangled >> 8);
-
-    // mangled *= BIT_NOISE1;
-    // mangled += seed;
-    // mangled ^= (mangled << 13);
-    // // mangled ^= (mangled >> 8);
-    // mangled += BIT_NOISE2;
-    // mangled ^= (mangled >> 7);
-    // // mangled ^= (mangled << 8);
-    // mangled *= BIT_NOISE3;
-    // mangled ^= (mangled << 17);
 
     return mangled;
 }
@@ -135,10 +138,8 @@ void flat_number_gen(
 
         if(!in){
             numbers.push_back((T)num);
-            // std::cout << "\t" << num; 
         }
     }
-    // std::cout << std::endl;
 }
 
 
@@ -168,7 +169,6 @@ void grid_number_gen(
         }
     }
 }
-
 /*
     random number generator for different layouts dense and sparse
 */
@@ -184,6 +184,396 @@ void index_sparse(std::vector<size_t>&index, size_t distinct_values, size_t seed
     }
 }
 
+
+bool in_interval(size_t a, size_t s, size_t e){
+    if(s < e){
+        return a >= s && a <= e; 
+    }else if(s > e){
+        return a >= s || a <= e;
+    }else{
+        return true; // we have to assume that everything is meant
+    }
+}
+
+size_t find_place(bool * blocked, size_t HSIZE, size_t place, size_t elements_to_place){
+
+
+    size_t fist_blocked = HSIZE;
+    bool loop = true;
+    size_t etp = elements_to_place;
+    std::vector<size_t> empty_space;
+    std::vector<size_t> start_space;
+
+    size_t empty = 0;
+    size_t start = 0, end;
+
+    bool enough_space = false;
+    bool last_blocked = false;
+
+
+    // collect information about the layout in form of intervals for the given array
+    // this also includes if there even is a spot big enough for the placement
+    for(size_t i = 0; i < HSIZE; i++){
+        if(blocked[i]){
+            if(!last_blocked){
+                start_space.push_back(start);
+                empty_space.push_back(empty);
+                enough_space |= (empty >= elements_to_place);
+                last_blocked = true;
+            }
+        }else{
+            if(last_blocked){
+                start = i;
+                empty = 0;
+                last_blocked = false;
+            }
+            empty++;
+        }
+    }
+    if(empty_space.size() > 0 && !last_blocked){
+        empty_space[0] += empty;
+        start_space[0] = start;
+        enough_space |= (empty_space[0] >= elements_to_place);
+    }else if(!last_blocked){
+        empty_space.push_back(empty);
+        start_space.push_back(start);
+        enough_space |= (empty >= elements_to_place);
+    }
+    
+
+
+
+
+    for(size_t i = 0; i < start_space.size(); i++){
+        std::cout << i << ":\t" << start_space[i] << ",\t" << empty_space[i] << std::endl;
+    }
+
+
+    if(!enough_space){   // we have not enough space for this collision!
+        return HSIZE;
+    }
+    
+//Find the nearest spot on or after to start searching for placement    
+    size_t total_entries = start_space.size();
+    size_t entry = total_entries;
+    size_t entry_alt = total_entries;
+    for(size_t i = 0; i < start_space.size(); i++){
+        if(in_interval(place, start_space[i], (start_space[i] + empty_space[i] + HSIZE - 1 ) % HSIZE)){
+            entry = i;
+            break;
+        }
+        if(in_interval(place, start_space[i], HSIZE-1)){
+            entry_alt = i;
+        }
+    }
+
+    if(entry == total_entries){
+        entry = entry_alt;
+    }
+
+    entry %= empty_space.size();
+    size_t i = entry;
+    std::cout << "\nwish starting place: " << place << std::endl;
+    do{
+        std::cout << "\twish entry: " << entry << "\ttest entry: " << i << std::endl;
+        if(empty_space[i] >= elements_to_place){
+            start = start_space[i];
+            end = ((start + empty_space[i] + HSIZE - 1) % HSIZE);
+            std::cout << "\t\t[" << start << ", " << end << "]\n";
+            
+
+            size_t s2, e2;
+            bool s_okay = false, e_okay = false;
+            s2 = place;
+            e2 = (s2 + elements_to_place - 1) % HSIZE;
+            s_okay = in_interval(s2, start, end);
+            e_okay = in_interval(e2, start, end);
+            std::cout << "\t\t\t[" << s2 << ", " << e2 << "]\t" << s_okay << " " << e_okay << std::endl;
+
+            while(!s_okay || !e_okay){
+                if(s_okay && !e_okay){
+                    s2 += (HSIZE - 1); //save way to roll over.
+                }else{
+                    s2 += 1;
+                }
+                s2 %= HSIZE;
+                e2 = (s2 + elements_to_place - 1) % HSIZE;
+                s_okay = in_interval(s2, start, end);
+                e_okay = in_interval(e2, start, end);
+                std::cout << "\t\t\t[" << s2 << ", " << e2 << "]\t" << s_okay << " " << e_okay << std::endl;
+            }
+            std::cout << "FOUND A PLACE: " << s2 << std::endl;
+            return s2;
+        }
+        i = (i + 1) % empty_space.size();
+    }while(i != entry);
+
+    return 0; //some error occured
+    // gives us all the sizes of the concecutive empty spaces.
+
+}
+
+size_t make_place(size_t*& member_count, bool*& blocked, size_t HSIZE, size_t place, size_t elements_to_place){
+    // this method should only be used for the unaligned usecase. otherwise the alignment suffers.
+    
+    std::vector<size_t> used_space;
+    std::vector<size_t> start_used_space;
+
+    std::vector<size_t> empty_space;
+    std::vector<size_t> start_space;
+
+    size_t space_used_total = 0;
+    for(size_t i = 0; i < HSIZE; i++){
+        if(member_count[i] != 0){
+            start_used_space.push_back(i);
+            used_space.push_back(member_count[i]);
+            space_used_total += member_count[i];
+        }
+    }
+
+    if(used_space.size() <= 1 || elements_to_place + space_used_total > HSIZE){
+        // Problem with the hashtable. Only one entry and this method got called.
+        return HSIZE;
+    }
+
+    size_t start, end, empty, max = 0, max_pos;
+
+    for(size_t i = 0; i < start_used_space.size() - 1; i++){
+        start = start_used_space[i] + used_space[i];
+        end = start_used_space[i + 1];
+        empty = (((end + HSIZE) - start) + HSIZE) % HSIZE;
+        
+        if(max < empty){
+            max = empty;
+            max_pos = empty_space.size();
+        }
+
+        empty_space.push_back(empty);
+        start_space.push_back(start);
+    }
+    
+    size_t missing = elements_to_place - max;
+    std::vector<size_t> place_to_steal;
+
+    while(missing > 0){
+
+
+    }
+
+
+
+
+    return HSIZE;
+}
+
+
+void entry_place(size_t*& member_count, bool*& blocked, size_t HSIZE, size_t place, size_t elements_to_place){
+    std::cout << std::endl << "Write to place: " << place << " with a size of " << elements_to_place << std::endl;
+    std::cout << "start place:\t";
+    for(size_t i = 0; i < HSIZE; i++){
+        std::cout << blocked[i] << "  ";
+    }
+
+    size_t p = place%HSIZE;
+    for(size_t i = 0; i < elements_to_place; i++){
+        if(blocked[p]){
+            std::cout << std::endl << p << std::endl;
+            throw std::runtime_error("Bad entry location. Already used!");
+        }else{
+            blocked[p] = true;
+        }
+        p = (p+1) % HSIZE;
+    }
+    member_count[place] = elements_to_place;
+    
+    std::cout << std::endl << "end   place:\t";
+    for(size_t i = 0; i < HSIZE; i++){
+        std::cout << blocked[i] << "  ";
+    }
+    std::cout << std::endl << std::endl;
+}
+
+
+void place_collisions_unaligned(size_t*& individual_group_size, size_t*& member_count, bool*& blocked, size_t HSIZE, size_t groups, size_t to_place_total, size_t position){
+    size_t unused = HSIZE - to_place_total;
+    size_t groups_left = groups;
+    size_t still_to_place = to_place_total;
+    size_t running_pos = position % HSIZE;
+    size_t elements_to_place = 0;
+
+    size_t n_pos, k;
+    for(size_t i = 0; i < groups; i++){
+        elements_to_place = individual_group_size[i];
+        n_pos = find_place(blocked, HSIZE, running_pos, elements_to_place);
+        entry_place(member_count, blocked, HSIZE, n_pos, elements_to_place);
+
+        k = unused / groups_left;
+
+        running_pos = n_pos + elements_to_place + k;
+        unused -= k;
+        groups_left--;
+    }
+}
+
+void place_collisions_aligned(size_t*& individual_group_size, size_t*& member_count, bool*& blocked, size_t HSIZE, size_t groups, size_t to_place_total, size_t alignment){
+    size_t unused = HSIZE - to_place_total;
+    size_t groups_left = groups;
+    size_t still_to_place = to_place_total;
+    size_t running_pos = 0;
+    size_t elements_to_place = 0;
+    
+    size_t n_pos, k;
+    for(size_t i = 0; i < groups; i++){
+        elements_to_place = individual_group_size[i];
+        n_pos = find_place(blocked, HSIZE, running_pos, elements_to_place);
+        entry_place(member_count, blocked, HSIZE, n_pos, elements_to_place);
+
+        k = unused / groups_left;
+
+        running_pos = n_pos + elements_to_place + k;
+        unused -= k;
+        groups_left--;
+    }
+
+}
+
+
+//returns 0 iff their was a problem during generation
+// static perfect hashing (mod HSIZE)
+template<typename T>
+size_t generate_collision_data(
+    T*& result, 
+    size_t data_size,   // number of values to be generated
+    size_t distinct_values, // number of distinct values
+    size_t HSIZE, // hash_table_size
+    size_t collision_groups = 0,  // 0 means no collisions!
+    double collisions = 0.0, //[0, 1] 0 no collision 1 all collision
+    // Generation gen = Generation::FLAT,
+    Distribution dis = Distribution::UNIFORM,
+    Alignment ali = Alignment::UNALIGNED,
+    size_t allign_size = 16,
+    size_t start = 0,   // starting offset for consecutive numbers (dense)
+    size_t seed = 0     // for sparse number generation 0 true random, 1.. reproducible
+){
+    if(seed == 0){  
+        srand(std::time(nullptr));
+        seed = std::rand();
+    }
+    bool create_collisions = true;
+    if(collision_groups == 0){
+        collision_groups = 0;
+        collisions = 0;
+        create_collisions = false;
+    }
+    collisions = collisions <= 1 ? collisions : 1;
+    size_t collisions_total = distinct_values * collisions;
+    size_t non_collisions = distinct_values - collisions_total;
+
+    if(create_collisions && collisions_total/collision_groups < 2){
+        std::cout << "To many groups for the given collisions!\n";
+        return 0;
+    }
+
+    size_t *member_count = new size_t[HSIZE];
+    bool *blocked = new bool[HSIZE];
+
+    for(size_t i = 0; i < HSIZE; i++){
+        member_count[i] = 0;
+        blocked[i] = false;
+    }
+
+    if(create_collisions){
+        size_t *individual_group_size = new size_t[collision_groups];
+        
+        if(true){   //uniform
+            size_t help = collisions_total;
+            for(size_t i = collision_groups; i > 0; i--){
+                size_t current = help / i;
+                help -= current;
+                individual_group_size[i-1] = current;
+            }
+        }else if(false){    //quadratic //important for not enough collisions quadratic might act like uniform
+            size_t help = collisions_total;
+            for(size_t i = collision_groups; i > 0; i--){
+                size_t current = (help / ((1<<i) - 1.)) + 0.5;
+                current = current > 2 ? current : 2;
+                help -= current;
+                individual_group_size[i-1] = current;
+            }
+        }else{
+            return 0;
+        }
+
+        bool placed_all = false;
+        size_t to_place_id = 0;
+        size_t t = 0;
+
+        std::cout << collisions_total  << " collisions in " << collision_groups << " Groups\n";
+        for(size_t i = 0; i < collision_groups; i++){
+            std::cout << individual_group_size[i] << "\t";
+        }std::cout << std::endl << std::endl;
+
+        size_t space_wo_collisions = HSIZE - collisions_total;
+        size_t swo = space_wo_collisions, cgc = collision_groups;
+        
+        size_t elements_to_place = individual_group_size[to_place_id];
+        size_t place, next_place;
+            
+
+        switch (ali)
+        {
+            case Alignment::UNALIGNED:
+                place = noise(to_place_id + t, seed)  % HSIZE;
+
+                place_collisions_unaligned(individual_group_size, member_count, blocked, HSIZE, collision_groups, collisions_total, place);
+
+            case Alignment::GOOD:   // try to place the blocks as good as possible for avx/sse
+
+                break;
+            case Alignment::BAD:    // try to place the blocks as badly as possible for avx/sse
+                break;
+            default:
+                break;
+        }
+
+        do{
+            switch (ali)
+            {
+            case Alignment::UNALIGNED:
+                place = noise(to_place_id + t, seed)  % HSIZE;
+                next_place = find_place(blocked, HSIZE, place, elements_to_place);
+                if(next_place >= HSIZE){ //we need to "defragment" our hash table other wise we might create more complex collisions
+                    next_place = make_place(member_count, blocked, HSIZE, place, elements_to_place);
+                }
+                entry_place(member_count, blocked, HSIZE, next_place, elements_to_place);
+                to_place_id++;
+                break;
+
+            case Alignment::GOOD:   // try to place the blocks as good as possible for avx/sse
+
+                break;
+            case Alignment::BAD:    // try to place the blocks as badly as possible for avx/sse
+                break;
+            default:
+                break;
+            }
+            t++;
+        }while(to_place_id < collision_groups);
+        
+
+
+
+    
+    }
+
+
+
+
+
+
+
+    return seed;
+}
 
 /*
     Data generator with different options for data layout. 
---
+++ hashing_testing/src/main/general.hpp
@@ -0,0 +1,294 @@
+#include <iostream>
+#include <fstream>
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <vector>
+#include <cstdlib>
+#include <chrono>
+
+
+#include "../operator/physical/group_count/scalar_group_count.hpp"
+#include "../operator/physical/group_count/avx512_group_count_soa_v1.hpp"
+#include "../operator/physical/group_count/avx512_group_count_soa_v2.hpp"
+#include "../operator/physical/group_count/avx512_group_count_soa_v3.hpp"
+#include "../operator/physical/group_count/avx512_group_count_soaov_v1.hpp"
+
+/// @brief Executes the hash function and collecting performance Data. 
+/// @tparam T 
+/// @param group_count The group_count operation that shall be executed.
+/// @param data The data on which the operation shall be evaluated
+/// @param data_size 
+/// @param validation_value //Information for validation Key column
+/// @param validation_count //Information for validation count column
+/// @param validation_size 
+/// @param cleanup true if group_count should be delete when the benchmark is finished. 
+template <typename T>
+size_t run_test(Group_count<T>* group_count, T* data, size_t data_size, T* validation_value, T* validation_count, size_t validation_size, bool cleanup){
+// prepare for the testing of the function    
+    std::chrono::high_resolution_clock::time_point time_begin, time_end;
+    uint64_t duration;
+    double duration_s;
+    double data_amount = (data_size * sizeof(ps_type) * 8)/1000000000.0; // Gbit
+    double data_count = data_size / 1000000000.0; // Million Values
+
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+    std::cout << group_count->identify() << std::endl;
+// run the test and time it
+    time_begin = time_now();
+    group_count->create_hash_table(data, data_size);;
+    time_end = time_now();
+    
+    std::cout << "\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+// out put result
+    duration = duration_time(time_begin, time_end);
+    duration_s = duration / 1000000000.0;
+
+    std::cout << "\tTime:\t" << duration << " ns\tOR\t" << duration_s << " s\n";
+    std::cout << "\tData:\t" << data_amount << " Gbit\tOR\t" << data_size << " Values\n"; 
+    std::cout << "\ttput:\t" << (data_amount)/(duration_s) << " Gbit/s\tor\tperf:\t" << (data_count)/(duration_s) << " Gval/s\n";
+    std::cout << "\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+// validate run
+    bool errors = validation<ps_type>(group_count, validation_value, validation_count, validation_size);
+
+    if(cleanup){
+        delete group_count;
+    }
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
+
+    if(errors){
+        return 0;
+    }
+    return duration;
+}
+
+
+/// @brief Executes the hash function and collecting performance Data. 
+/// @tparam T 
+/// @param group_count The group_count operation that shall be executed.
+/// @param data The data on which the operation shall be evaluated
+/// @param data_size 
+/// @param validation_baseline annother run of the scalar algorithm to compare the results.
+/// @param validation_size the hash table size of the validation_baseline
+/// @param cleanup true if group_count should be delete when the benchmark is finished. 
+template <typename T>
+size_t run_test(Group_count<T>* group_count, T* data, size_t data_size, Scalar_group_count<T>* validation_baseline, size_t validation_size, bool validate, bool cleanup){
+// prepare for the testing of the function    
+    std::chrono::high_resolution_clock::time_point time_begin, time_end;
+    uint64_t duration;
+    double duration_s;
+    double data_amount = (data_size * sizeof(ps_type) * 8)/1000000000.0; // Gbit
+    double data_count = data_size / 1000000000.0; // Million Values
+
+    // std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+    // std::cout << group_count->identify() << std::endl;
+// run the test and time it
+    time_begin = time_now();
+    group_count->create_hash_table(data, data_size);;
+    time_end = time_now();
+    
+    // std::cout << "\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+// out put result
+    duration = duration_time(time_begin, time_end);
+    duration_s = duration / 1000000000.0;
+
+    // std::cout << "\tTime:\t" << duration << " ns\tOR\t" << duration_s << " s\n";
+    // std::cout << "\tData:\t" << data_amount << " Gbit\tOR\t" << data_size << " Values\n"; 
+    // std::cout << "\ttput:\t" << (data_amount)/(duration_s) << " Gbit/s\tor\tperf:\t" << (data_count)/(duration_s) << " Gval/s\n";
+    // std::cout << "\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+// validate run
+    bool errors = false;
+    if(validate){
+        errors = validation<ps_type>(group_count, validation_baseline, validation_size);
+    }
+
+    if(cleanup){
+        delete group_count;
+    }
+    // std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
+
+    if(errors){
+        throw std::runtime_error("Problem during Validation!");
+    }
+    return duration;
+}
+
+/*
+    creates a time point
+*/
+std::chrono::high_resolution_clock::time_point time_now(){
+    return std::chrono::high_resolution_clock::now();
+}
+
+/*
+    gives the time between begin and end in nanoseconds
+*/
+uint64_t duration_time (std::chrono::high_resolution_clock::time_point begin, std::chrono::high_resolution_clock::time_point end){
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+
+}
+
+/// @brief Creates a arrays with the expected result values, that can be used to evaluate
+/// @tparam T 
+/// @param res_table pointer to the result array containing all values
+/// @param res_count pointer to the result array containing the counts
+/// @param data array contaning all the values
+/// @param data_size number of entries in the data array
+/// @param HSIZE the size of the Hash table 
+/// @return the number of slots used in the result arrays
+template <typename T> 
+size_t createCountValidationTable(T** res_table, T** res_count, T* data, size_t data_size, size_t HSIZE){
+    *res_table = new T[HSIZE];
+    *res_count = new T[HSIZE];
+    size_t m_id = 0;
+    
+    for(size_t p = 0; p < data_size; p++){
+        T value = data[p];
+        bool found = false;
+        for(size_t i = 0; i < m_id; i++){
+            if((*res_table)[i] == value){
+                (*res_count)[i]++;
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            (*res_table)[m_id] = value;
+            (*res_count)[m_id] = 1;
+            m_id++;
+        }
+    }
+    return m_id;
+}
+
+template <typename T> 
+bool validation(Group_count<T>* grouping, T* table_value, T* table_count, size_t data_size){
+    std::cout << "Start Validation";
+    size_t nr_of_errors = 0;
+    for(size_t i = 0; i < data_size; i++){
+        T value = table_value[i];
+        size_t count = grouping->get(value);
+
+        if(count != table_count[i]){
+            if(nr_of_errors == 0){
+                std::cout << std::endl;
+            }
+
+            nr_of_errors++;
+            std::cout << "\tERROR Count\t" << value << " has a count of\t" 
+                << count << " but expected to have\t" << table_count[i] << std::endl;
+        }
+    }
+    if(nr_of_errors == 1){
+        std::cout << "\tFound one Error\n";
+    }else if(nr_of_errors > 1){
+        std::cout << "\tFound " << nr_of_errors << " Errors\n";
+    }else{
+        std::cout << "\t---\t";
+    }
+    std::cout << "End of Validation\n";
+    return nr_of_errors != 0;
+}
+
+template <typename T> 
+bool validation(Group_count<T>* grouping, Group_count<T> *validation_baseline, size_t validation_size){
+    std::cout << "Start Validation";
+    size_t nr_of_errors = 0;
+    for(size_t i = 0; i < validation_size; i++){
+        T value = validation_baseline->getval(i);
+        if(value != 0){
+            T expected_count = validation_baseline->get(value);
+            T result_count = grouping->get(value);
+            if(expected_count != result_count){
+                if(nr_of_errors == 0){
+                    std::cout << std::endl;
+                }
+
+                nr_of_errors++;
+                std::cout << "\tERROR Count\t" << value << " has a count of\t" 
+                    << result_count << " but expected to have\t" << expected_count << std::endl;
+            }
+        }
+    }
+    if(nr_of_errors == 1){
+        std::cout << "\tFound one Error\n";
+    }else if(nr_of_errors > 1){
+        std::cout << "\tFound " << nr_of_errors << " Errors\n";
+    }else{
+        std::cout << "\t---\t";
+    }
+    std::cout << "End of Validation\n";
+    return nr_of_errors != 0;
+}
+
+
+
+//---------------------------------------
+// output functions
+//---------------------------------------
+
+void create_result_file(std::string filename){
+    std::ofstream myfile;
+    myfile.open (filename);
+    if(myfile.is_open()){
+        myfile << "Algorithm,time,data size,bytes,distinct value count,scale,hash table size,hash function ID,seed,run ID\n";
+        myfile.close();
+    } else {
+        throw std::runtime_error("Could not open file to write results!");
+    }
+}
+
+void write_to_file( std::string filename, //string
+    std::string alg_identification, //string
+    // benchmark time
+    uint64_t time, //size_t or uint64_t
+    // config
+    size_t data_size,   // size_t 
+    size_t bytes,
+    size_t distinct_value_count, // size_t 
+    float scale, // Scaleing factor for Hash Table double/float
+    size_t HSIZE, // HASH Table Size size_t 
+    size_t ahfs, // hash function index size_t 
+    size_t seed, // Datageneration seed COULD BE REPLACED BY ANNOTHER ID BUT!  size_t 
+    size_t rsd // run id (same config with same runs) size_t 
+){
+    std::ofstream myfile;
+    myfile.open (filename, std::ios::app);
+    if(myfile.is_open()){
+        // "Algorithm,time,data size,bytes,distinct value count,scale,hash table size,hash function ID,seed,run ID";
+        myfile << alg_identification << "," << time << "," << data_size << "," << bytes << "," << distinct_value_count  << "," 
+            << scale << "," << HSIZE << "," << ahfs << "," << seed << "," << rsd  << "\n"; 
+        myfile.close();
+    } else {
+        throw std::runtime_error("Could not open file to write results!");
+    }
+//std::cout << alg_identification << "\t" << time << "\t" << rsd << "\t" << scale << "\t" << distinct_value_count << "\t" << seed << "\t" << data_size << std::endl;
+}
+
+
+
+
+//---------------------------------------
+// hash function
+//---------------------------------------
+
+/// @brief This "Hash" functions results always in a collition
+/// @param key  Key gets ignored but is needed for the function template
+/// @param HSIZE Hash Table size. 
+/// @return value range in [0:HSIZE-1]
+template <typename T>
+size_t force_collision(T key, size_t HSIZE){
+    return HSIZE - 1;
+}
+
+// simple multiplicative hashing function
+size_t hashx(uint32_t key, size_t HSIZE) {
+    return ((unsigned long)((unsigned int)1300000077*key)* HSIZE)>>32;
+}
+
+template<typename T>
+size_t id_mod(T key, size_t HSIZE) {
+    return key % HSIZE;
+}
