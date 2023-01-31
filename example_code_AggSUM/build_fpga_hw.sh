819b7238082714c94929bf9563e2f8d333da7f1c
eric-stange <Eric.Stange@Mailbox.Tu-Dresden.de>
2023-01-31 08:15:25 -0800

    add error logs of example_code_AggSUM project

--- example_code_AggSUM/build_fpga_hw.sh
+++ example_code_AggSUM/build_fpga_hw.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
+# # run this by
+# qsub -l nodes=1:fpga_compile:ppn=2 -d . build_fpga_emu.sh -l walltime=23:00:00
+# # monitor job via
+# watch -n 1 qstat -n -1
+
+# Initial Setup
 source /data/intel_fpga/devcloudLoginToolSetup.sh
 tools_setup -t S10OAPI
 export PATH=/glob/intel-python/python2/bin:$PATH
-source /opt/intel/inteloneapi/setvars.sh 
-source /glob/development-tools/versions/oneapi/2022.3/oneapi/setvars.sh --force
+
+cd ~/FPGA-SIMD-Sandbox/example_codeAggSUM 
+
+# Running project in FPGA Hardware Mode (this takes approximately 1 hour)
+printf "\\n%s\\n" "Running in FPGA Hardware compile Mode:"
 make hw
+# error_check
