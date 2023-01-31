bbd76499f9357708d648d5cf3324de2bb06744e2
eric-stange <Eric.Stange@Mailbox.Tu-Dresden.de>
2023-01-31 05:13:54 -0800

    add export PATH=/ ... command to build hw script

--- example_code_AggSUM/build_fpga_hw.sh
+++ example_code_AggSUM/build_fpga_hw.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-tools_setup -t S10OAPI
 source /data/intel_fpga/devcloudLoginToolSetup.sh
+tools_setup -t S10OAPI
+export PATH=/glob/intel-python/python2/bin:$PATH
 source /opt/intel/inteloneapi/setvars.sh 
 source /glob/development-tools/versions/oneapi/2022.3/oneapi/setvars.sh --force
 make hw
