f599e179584d8d1d2284168645ee2228099a9817
eric-stange <Eric.Stange@Mailbox.Tu-Dresden.de>
2023-02-03 05:07:35 -0800

    add readme, execute.sh to /hashing_testing_serialExecution/

--- hashing_testing_FPGA_serialExecution/build.sh
+++ hashing_testing_FPGA_serialExecution/build.sh
@@ -11,3 +11,10 @@ g++ -std=c++14 -O3 src/main/main.cpp -o test \
     src/operator/physical/group_count/fpga_group_count_soa_v2.cpp \
     src/operator/physical/group_count/fpga_group_count_soa_v3.cpp \
     src/operator/physical/group_count/fpga_group_count_soaov_v1.cpp
+
+# g++ -std=c++14 -O3 src/main/main_collision.cpp collision \
+#    src/operator/physical/group_count/scalar_group_count.cpp \
+#    src/operator/physical/group_count/fpga_group_count_soa_v1.cpp \
+#    src/operator/physical/group_count/fpga_group_count_soa_v2.cpp \
+#    src/operator/physical/group_count/fpga_group_count_soa_v3.cpp \
+#    src/operator/physical/group_count/fpga_group_count_soaov_v1.cpp
