b038fe434ee2342a0584deec6defb9047baaf0d2
eric-stange <Eric.Stange@Mailbox.Tu-Dresden.de>
2023-02-16 11:52:41 -0800

    edit build.sh script
    - describe different compile instructions

--- group_count_FPGA_serialExecution/build.sh
+++ group_count_FPGA_serialExecution/build.sh
@@ -5,7 +5,14 @@
 
 #cmake -S "$projectRoot" -B "$buildDir" 
 
- g++ -std=c++14 -O3 -o main src/main.cpp \
+# old  g++ -std=c++14 -O3 -o main src/main.cpp \ ...
+
+# new intel compiler with debug flags for gdb-oneapi debugger: icpx -fsycl -O3 -g -o main src/main.cpp \
+
+# new intel compiler:  icpx -fsycl -O3 -o main src/main.cpp \
+
+
+icpx -fsycl -O3 -g -o main src/main.cpp \
     src/helper_kernel.cpp \
     src/helper_main.cpp \
     src/kernel.cpp \
