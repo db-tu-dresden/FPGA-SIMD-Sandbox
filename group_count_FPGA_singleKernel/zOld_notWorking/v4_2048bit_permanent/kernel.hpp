9e37d8fad2c61fd4cb3a085d3299b73fed5de8b0
eric-stange <Eric.Stange@Mailbox.Tu-Dresden.de>
2023-03-11 08:56:27 -0800

    upload not working versions of v4 & v5
    - 2048bit_permanent implementations for these to versions are not working
    - make hw compile runs endless
    - to complex structure for FPGA?

---
+++ group_count_FPGA_singleKernel/zOld_notWorking/v4_2048bit_permanent/kernel.hpp
@@ -0,0 +1,19 @@
+#ifndef KERNEL_HPP__
+#define KERNEL_HPP__
+
+#include <CL/sycl.hpp>
+#include <sycl/ext/intel/fpga_extensions.hpp>
+
+#include "lib/lib.hpp"
+#include "global_settings.hpp"
+#include "primitives.hpp"
+
+using namespace sycl;
+
+class kernelV4;
+
+void LinearProbingFPGA_variant4(queue& q, uint32_t *arr_d, uint32_t *hashVec_d, uint32_t *countVec_d, uint64_t dataSize, uint64_t HSIZE, size_t size,  
+                                size_t m_elements_per_vector, size_t m_HSIZE_v, size_t m_HSIZE);
+
+
+#endif  // KERNEL_HPP__
