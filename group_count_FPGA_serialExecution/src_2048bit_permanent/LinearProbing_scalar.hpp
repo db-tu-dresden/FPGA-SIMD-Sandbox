bfd33f60a5ed1186a29fa74959f6ab5de2417041
eric-stange <Eric.Stange@Mailbox.Tu-Dresden.de>
2023-04-04 06:48:05 -0700

    Restructuring repository for eval #2

--- group_count_FPGA_serialExecution/src_2048bit_permanent/LinearProbing_scalar.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef LinearProbingScalar_HPP
-#define LinearProbingScalar_HPP
-
-class kernelScalar;
-
-
-void LinearProbingScalar(uint32_t* input, uint32_t* hashVec, uint32_t* countVec);
-
-#endif  // LinearProbingScalar_HPP
